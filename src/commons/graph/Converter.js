f16442b6d973793c9fc9aa0bef98f17e98cffbcf
JoshCunningHum <jkdimpas@gmail.com>
2023-10-15 14:01:06 +0800

    Prioritize looping systems first

--- src/commons/graph/Converter.js
+++ src/commons/graph/Converter.js
@@ -32,23 +32,23 @@ export class FSM {
     /**@type {FSMData} */
     data;
 
-    get states(){
+    get states() {
         return this.data.states;
     }
 
-    get initial(){
+    get initial() {
         return this.data.initialState;
     }
 
-    get accepting(){
+    get accepting() {
         return this.data.acceptingStates;
     }
-    
-    get transitions(){
+
+    get transitions() {
         return this.data.transitions;
     }
 
-    get alphabet(){
+    get alphabet() {
         return this.data.alphabet;
     }
 
@@ -57,229 +57,265 @@ export class FSM {
         const fi = this.index_s(from),
             ti = this.index_s(to);
 
-        if(fi === -1 || ti === -1) return;
+        if (fi === -1 || ti === -1) return;
 
         // console.log(from, to, value);
 
         // Check if there is already an existing transition
-        const i = this.transitions.findIndex(t => t.fromState == from && t.symbol === value);
-        const t = i === -1 ? new FSMTransition(from, value, to) : this.transitions[i];
+        const i = this.transitions.findIndex(
+            (t) => t.fromState == from && t.symbol === value
+        );
+        const t =
+            i === -1 ? new FSMTransition(from, value, to) : this.transitions[i];
         if (i !== -1) t.toStates = [...t.toStates, to];
         else this.transitions.push(t);
-    }
-    index_tfrom = from => this.transitions.findIndex(t => t.fromState === from);
-    index_tto = to => this.transitions.findIndex(t => t.toStates.includes(to));
-    remove_tfrom = from => {
+    };
+    index_tfrom = (from) =>
+        this.transitions.findIndex((t) => t.fromState === from);
+    index_tto = (to) =>
+        this.transitions.findIndex((t) => t.toStates.includes(to));
+    remove_tfrom = (from) => {
         let n;
-        while(n !== -1){
-            n = this.index_tfrom(from)
-            if(n === -1) break;
+        while (n !== -1) {
+            n = this.index_tfrom(from);
+            if (n === -1) break;
             this.transitions.splice(n, 1);
         }
-    }
-    remove_tto = to => {
+    };
+    remove_tto = (to) => {
         let n;
-        while(n !== -1){
+        while (n !== -1) {
             n = this.index_tto(to);
-            if(n == -1) break;
+            if (n == -1) break;
             const t = this.transitions[n];
-            if(t.toStates.length === 1) this.transitions.splice(n, 1);
+            if (t.toStates.length === 1) this.transitions.splice(n, 1);
             else t.toStates.splice(t.toStates.indexOf(to), 1);
         }
-    }
-    remove_tall = state => {
+    };
+    remove_tall = (state) => {
         this.remove_tfrom(state);
         this.remove_tto(state);
-    }
+    };
 
     /**
-     * 
-     * @param {String} from 
-     * @param {String} to 
+     *
+     * @param {String} from
+     * @param {String} to
      * @returns {String[]}
      */
     t = (from, to, filter = false) => {
-        return this.transitions.filter(t => t.fromState === from && t.toStates.includes(to)).map(t => t.symbol).filter(t => !filter || t !== '');
-    }
+        return this.transitions
+            .filter((t) => t.fromState === from && t.toStates.includes(to))
+            .map((t) => t.symbol)
+            .filter((t) => !filter || t !== "");
+    };
 
     /**
-     * 
-     * @param {String} label 
+     *
+     * @param {String} label
      * @returns {Array}
      */
-    tout = label => {
-        return this.transitions.filter(t => t.fromState === label);
-    }
+    tout = (label) => {
+        return this.transitions.filter((t) => t.fromState === label);
+    };
 
     /**
-     * 
-     * @param {String} label 
+     *
+     * @param {String} label
      * @returns {Array}
      */
-    tins = label => {
-        return this.transitions.filter(t => t.toStates.includes(label));
-    }
+    tins = (label) => {
+        return this.transitions.filter((t) => t.toStates.includes(label));
+    };
 
     /**
-     * 
-     * @param {String} label 
+     *
+     * @param {String} label
      * @returns {Array}
      */
-    lout = label => {
+    lout = (label) => {
         const res = new Set();
-        this.tout(label).forEach(t => t.toStates.forEach(l => l !== label && res.add(l)));
+        this.tout(label).forEach((t) =>
+            t.toStates.forEach((l) => l !== label && res.add(l))
+        );
         return [...res.values()];
-    }
+    };
 
     /**
-     * 
-     * @param {String} label 
+     *
+     * @param {String} label
      * @returns {Array}
      */
-    lins = label => {
+    lins = (label) => {
         const res = new Set();
-        this.tins(label).forEach(({fromState: from}) => from !== label && res.add(from));
+        this.tins(label).forEach(
+            ({ fromState: from }) => from !== label && res.add(from)
+        );
         return [...res.values()];
-    }
+    };
 
-    add_s = label => this.states.push(label);
-    index_s = label => this.states.findIndex(s => s == label);
-    remove_s = label => {
+    add_s = (label) => this.states.push(label);
+    index_s = (label) => this.states.findIndex((s) => s == label);
+    remove_s = (label) => {
         const n = this.index_s(label);
-        if(n === -1) return;
+        if (n === -1) return;
         // Remove all transitions that contains this state
         this.remove_tall(label);
 
         this.states.splice(n, 1);
+    };
 
-    }
-
-
-
-    constructor(data){
-        if(typeof data === 'string') data = noam.fsm.parseFsmFromString(data);
+    constructor(data) {
+        if (typeof data === "string") data = noam.fsm.parseFsmFromString(data);
         this.data = data;
     }
 
-    toRegex(){
-        // Do validation 
+    toRegex() {
+        // Do validation
         this.data = noam.fsm.removeUnreachableStates(this.data);
-        if(!noam.fsm.validate(this.data)) {
-            console.log('FSM Not Valid');
+        if (!noam.fsm.validate(this.data)) {
+            console.log("FSM Not Valid");
 
             return;
         }
 
         const { states, accepting, transitions, initial } = this;
 
-        const start = '__s__',
-            end = '__e__';
+        const start = "__s__",
+            end = "__e__";
 
         // Add Start and Ending states
         this.add_s(start);
         this.add_s(end);
-        this.add_t(start, initial, '');
-        accepting.forEach(a => this.add_t(a, end, ''));
+        this.add_t(start, initial, "");
+        accepting.forEach((a) => this.add_t(a, end, ""));
         this.data.initialState = start;
         this.data.acceptingStates = [end];
 
         let counter = 0; // To avoid infinite loop
-        while (states.length > 2 && counter < states.length){
+        while (states.length > 2 && counter < states.length) {
             // Get Intermeddiate of start
-            const imd = this.lout(start).filter(l => this.hasPath(l, end));
+            const imd = this.lout(start).filter((l) => this.hasPath(l, end));
 
-            imd.forEach(state => {
-                if(state === end) return;
-
-                const outs = this.lout(state).filter(l => this.hasPath(l, end)),
-                    ins = this.lins(state).filter(l => this.hasPath(state, l));
-
-                // Handle Outgoings
-                outs.forEach(s => {
-                    if(s === state) return;
-                    if(!this.hasPath(s, end)){
-                        this.remove_s(s); // Remove Dead State
-                        return;
-                    };
-
-                    const pathToEnd = this.path(s, end).slice(1, -1);
-
-                    if(pathToEnd.includes(state)) console.log(`State ${s} is inside a looping system which entrance/exit is ${state}`);
-
-                    const ft = this.wrap(this.t(start, state), '+');
-                    const sl = this.wrap(this.t(state, state), '*');
-                    const et = this.wrap(this.t(state, s), '+');
-                    this.add_t(start, s, ft + sl + et);
-                })
-
-                // Handle Ingoings
-                ins.forEach(s => {
-                    if(s === state && s === start) return;
-
-                    const targets = outs;   
-
-                    targets.forEach(d => {
-                        const ts = this.wrap(this.t(s, state), '+');
-                        const sl = this.wrap(this.t(state, state), '*');
-                        const sd = this.wrap(this.t(state, d), '+');
-
-                        this.add_t(s, d, ts + sl + sd);
-                    })
-                })
-
-                this.remove_s(state);
-                // console.log(state);
-                // console.log(structuredClone(this.data));
-            })
+            imd.forEach((state) => {
+                if (state === end) return;
+                this.stateEliminateDelete(state, start, end);
+            });
 
             counter++;
         }
 
-        return this.data.transitions.filter(t => t.fromState === start && t.toStates.includes(end)).map(t => t.symbol === '' ? '$' : t.symbol).join('+');
+        return this.data.transitions
+            .filter((t) => t.fromState === start && t.toStates.includes(end))
+            .map((t) => (t.symbol === "" ? "$" : t.symbol))
+            .join("+");
     }
 
-    stateEliminateDelete(state){
+    stateEliminateDelete(state, start, end) {
+        // console.log(`Deleting ${state}`);
 
+        const transitions = this.transitions;
+
+        const outs = this.lout(state).filter((l) => {
+                const isNotDeadState = this.hasPath(l, end);
+                if (!isNotDeadState) this.remove_s(l);
+                return isNotDeadState;
+            }),
+            ins = this.lins(state);
+
+        // console.log(`Transitions: `, transitions);
+        // console.log(`Outs: `, outs);
+        // console.log(`Ins: `, ins);
+
+        // Find looping systems
+        for (let i = 0; i < outs.length; i++) {
+            const s = outs[i];
+
+            if (s === state) return;
+
+            const pathToEnd = this.path(s, end).slice(1, -1);
+            if (pathToEnd.includes(state)) {
+                // console.log(`Looping: ${s} Entrance: ${state}`);
+                // Delete the state "s" first before continuing
+                this.stateEliminateDelete(s, start, end);
+                // Remove S in the outgoings and ingoings (if found)
+                outs.splice(i, 1); i--;
+                const insIndex = ins.indexOf(s);
+                if(insIndex !== -1) ins.splice(insIndex, 1);
+            }
+        }
+
+        // Handle Ingoings
+        ins.forEach((s) => {
+            if (s === state) return;
+
+            const targets = outs;
+
+            targets.forEach((d) => {
+                const ts = this.wrap(this.t(s, state), "+");
+                const sl = this.wrap(this.t(state, state), "*");
+                const sd = this.wrap(this.t(state, d), "+");
+
+                this.add_t(s, d, ts + sl + sd);
+            });
+        });
+
+        this.remove_s(state);
+        // console.log(state);
+        // console.log(structuredClone(this.data));
     }
 
-    wrap(transitions, operation){
-        if(operation === '+'){
-            return transitions.length === 1 ? transitions[0] :
-                transitions.length > 1 ? this.wrapIfNot(transitions.join(operation)) : '';
-        }else if(operation === '*'){
-            return transitions.length === 1 ? `${transitions[0].length === 1 ? transitions : `${this.wrapIfNot(transitions[0])}`}*` :
-                transitions.length > 1 ? `${this.wrapIfNot(this.wrap(transitions, '+'))}*` : '';
+    wrap(transitions, operation) {
+        if (operation === "+") {
+            return transitions.length === 1
+                ? transitions[0]
+                : transitions.length > 1
+                ? this.wrapIfNot(transitions.join(operation))
+                : "";
+        } else if (operation === "*") {
+            return transitions.length === 1
+                ? `${
+                      transitions[0].length === 1
+                          ? transitions
+                          : `${this.wrapIfNot(transitions[0])}`
+                  }*`
+                : transitions.length > 1
+                ? `${this.wrapIfNot(this.wrap(transitions, "+"))}*`
+                : "";
         }
     }
 
     /**
-     * 
-     * @param {String} str 
-     * @returns 
+     *
+     * @param {String} str
+     * @returns
      */
-    wrapIfNot(str){
-         if(str[0] === '(' && str.endsWith(')')) return str;
-         else return `(${str})`;
+    wrapIfNot(str) {
+        if (str[0] === "(" && str.endsWith(")")) return str;
+        else return `(${str})`;
     }
 
-    hasPath(from, to){
-        const nvs = [from], vs = new Set();
+    hasPath(from, to) {
+        const nvs = [from],
+            vs = new Set();
 
         let counter = 0;
-        while(nvs.length > 0){
+        while (nvs.length > 0) {
             const s = nvs.shift();
-            if(s === to) return true;
+            if (s === to) return true;
             vs.add(s);
 
             const outs = new Set();
 
-            this.tout(s).forEach(os => os.toStates.forEach(l => !vs.has(l) && outs.add(l)));
+            this.tout(s).forEach((os) =>
+                os.toStates.forEach((l) => !vs.has(l) && outs.add(l))
+            );
 
             nvs.push(...outs.values());
             counter++;
 
-            if(counter > this.states.length ** 2){
-                console.log('BROKE PATHFINDING');
+            if (counter > this.states.length ** 2) {
+                console.log("BROKE PATHFINDING");
                 break;
             }
         }
@@ -287,43 +323,51 @@ export class FSM {
         return false;
     }
 
-    path(from, to){
-        const nvs = [{
-            label: from,
-            from: ''
-        }], vs = [];
+    path(from, to) {
+        const nvs = [
+                {
+                    label: from,
+                    from: "",
+                },
+            ],
+            vs = [];
 
         let counter = 0;
-        while(nvs.length > 0){
+        while (nvs.length > 0) {
+            const { label: s, from: prev } = nvs.shift();
+            vs.push({ label: s, from: prev });
 
-            const {label: s, from: prev} = nvs.shift();
-            vs.push({label: s, from: prev});
-
-            if(s === to) {
-                
-                let target = to, path = [to];
-                while(target !== from){
-                    const { from } = vs.find(v => v.label === target);
+            if (s === to) {
+                let target = to,
+                    path = [to];
+                while (target !== from) {
+                    const { from } = vs.find((v) => v.label === target);
                     path.unshift(from);
                     target = from;
                 }
 
                 return path;
-            };
+            }
 
             const outs = new Set();
 
-            this.tout(s).forEach(os => os.toStates.forEach(l => !vs.find(v => v.label === l) && outs.add(l)));
-            nvs.push(...[...outs.values()].map(v => {
-                return {
-                    label: v,
-                    from: s
-                }
-            }));
+            this.tout(s).forEach((os) =>
+                os.toStates.forEach(
+                    (l) => !vs.find((v) => v.label === l) && outs.add(l)
+                )
+            );
+            nvs.push(
+                ...[...outs.values()].map((v) => {
+                    return {
+                        label: v,
+                        from: s,
+                    };
+                })
+            );
             counter++;
 
-            if(counter > this.states.length ** 2){
-                console.log('BROKE PATHFINDING');
+            if (counter > this.states.length ** 2) {
+                console.log("BROKE PATHFINDING");
                 break;
             }
         }
